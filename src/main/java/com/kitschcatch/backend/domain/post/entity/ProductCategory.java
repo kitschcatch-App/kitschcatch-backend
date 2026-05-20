3d79c45ab0b8c6685f3850e79746c979673e16df
kimhoeyun <clnvrd@suwon.ac.kr>
2026-05-20 12:52:26 +0900

    11 feat: 상품 종류 카테고리 enum 추가

--- src/main/java/com/kitschcatch/backend/domain/post/entity/ProductCategory.java
+++ src/main/java/com/kitschcatch/backend/domain/post/entity/ProductCategory.java
@@ -1,9 +1,24 @@
 package com.kitschcatch.backend.domain.post.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ProductCategory {
-	FIGURE,
-	PLUSH,
-	KEYRING,
-	CARD,
-	ETC
+	ANIME_MANGA("애니/만화"),
+	GAME("게임"),
+	GOODS("굿즈"),
+	COSPLAY("코스프레"),
+	BOOK("서적"),
+	MUSIC_VIDEO("음반/영상"),
+	ETC("기타");
+
+	private final String label;
+
+	ProductCategory(String label) {
+		this.label = label;
+	}
+
+	@JsonValue
+	public String getLabel() {
+		return label;
+	}
 }
