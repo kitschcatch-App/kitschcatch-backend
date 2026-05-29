d0905a129440d5ed0e8bf39451105e11cc35d681
hanjh <wg1004s@naver.com>
2026-05-29 19:28:15 +0900

    15 fix: 환경 변수 예시 파일 및 import 정리

--- src/main/java/com/kitschcatch/backend/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/kitschcatch/backend/domain/chat/entity/ChatRoom.java
@@ -2,7 +2,18 @@ package com.kitschcatch.backend.domain.chat.entity;
 
 import com.kitschcatch.backend.domain.post.entity.Post;
 import com.kitschcatch.backend.domain.user.entity.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
